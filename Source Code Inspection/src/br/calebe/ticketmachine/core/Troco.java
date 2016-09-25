6ef8669373031d4acd6e5e04bf536fc32a373e9f
Carol <Carol@Carol-PC>
2016-09-25 16:47:41 -0300

    Terceiro commit correção Commit de correção das issues detectadas

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -12,36 +12,21 @@ class Troco {
 
     public Troco(int valor) {
         papeisMoeda = new PapelMoeda[6];
-        int count = 0;
-        while (valor % 100 != 0) {
-            count++;
+        int[] tipoPapelMoeda = {2, 5, 10, 20, 50, 100};
+
+        for (int i = 0; valor > 0; i++) {
+            int count = 0;
+            boolean achou = false;
+            for (int j = 0; j < tipoPapelMoeda.length && !achou; j++) {
+                if (tipoPapelMoeda[i] == valor) {
+                    while (valor % tipoPapelMoeda[i] != 0) {
+                        count++;
+                    }
+                    papeisMoeda[i] = new PapelMoeda(tipoPapelMoeda[i], count);
+                    valor = valor - (tipoPapelMoeda[i] * count);
+                }
+            }
         }
-        papeisMoeda[5] = new PapelMoeda(100, count);
-        count = 0;
-        while (valor % 50 != 0) {
-            count++;
-        }
-        papeisMoeda[4] = new PapelMoeda(50, count);
-        count = 0;
-        while (valor % 20 != 0) {
-            count++;
-        }
-        papeisMoeda[3] = new PapelMoeda(20, count);
-        count = 0;
-        while (valor % 10 != 0) {
-            count++;
-        }
-        papeisMoeda[2] = new PapelMoeda(10, count);
-        count = 0;
-        while (valor % 5 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(5, count);
-        count = 0;
-        while (valor % 2 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(2, count);
     }
 
     public Iterator<PapelMoeda> getIterator() {
