c54de63436375b1c177b2a83a358e23bc44eb734
Ana Carolina dos Santos Silva <ana.carolina_95@hotmail.com>
2016-10-07 20:22:22 -0300

    Add files via upload

---
+++ Source%20Code%20Inspection/test/br/calebe/ticketmachine/core/TicketMachineTest.java
@@ -0,0 +1,66 @@
+package br.calebe.ticketmachine.core;
+
+import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
+import java.util.Iterator;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Bárbara
+ */
+public class TicketMachineTest {
+
+    public TicketMachineTest() {
+    }
+
+    private TicketMachine ticketMachine;
+
+    @Before
+    public void criandoTicketMachine() {
+        ticketMachine = new TicketMachine(20);
+    }
+
+    @Test
+    public void inserirNotaDe2() throws PapelMoedaInvalidaException {
+        ticketMachine.inserir(2);
+        assertEquals(ticketMachine.getSaldo(), 2);
+    }
+
+    @Test
+    public void inserirNotaDe5() throws PapelMoedaInvalidaException {
+        ticketMachine.inserir(5);
+        assertEquals(ticketMachine.getSaldo(), 5);
+    }
+
+    @Test
+    public void inserirNotaDe10() throws PapelMoedaInvalidaException {
+        ticketMachine.inserir(10);
+        assertEquals(ticketMachine.getSaldo(), 10);
+    }
+
+    @Test
+    public void inserirNotaDe20() throws PapelMoedaInvalidaException {
+        ticketMachine.inserir(20);
+        assertEquals(ticketMachine.getSaldo(), 20);
+    }
+    
+    @Test
+    public void inserirNotaDe50() throws PapelMoedaInvalidaException {
+        ticketMachine.inserir(50);
+        assertEquals(ticketMachine.getSaldo(), 50);
+    }
+    
+    @Test
+    public void inserirNotaDe100() throws PapelMoedaInvalidaException {
+        ticketMachine.inserir(100);
+        assertEquals(ticketMachine.getSaldo(), 100);
+    }
+    
+    @Test
+    public void inserirNotaInvalida() throws PapelMoedaInvalidaException {
+        ticketMachine.inserir(3);
+        assertEquals(ticketMachine.getSaldo(), 3);
+    }
+}
